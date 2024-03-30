b5e059b5684e291deecb43783c4b50b1868d9335
Kyle Nathan Naranjo <63973445+teslakoile@users.noreply.github.com>
2024-03-30 21:52:17 +0800

    feat: register BlogPost in admin

--- BlogMaker/blog_backend/admin.py
+++ BlogMaker/blog_backend/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import BlogPost
 
 # Register your models here.
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'updated_at', 'snippet')
+    readonly_fields = ('id', 'created_at', 'updated_at', 'snippet')
+    
+    def snippet(self, obj):
+        """
+        Returns the snippet property of the BlogPost object.
+        """
+        return obj.snippet
