1b50b77de150c11d8fd7b95617bd2d4f84739396
Kyle Nathan Naranjo <63973445+teslakoile@users.noreply.github.com>
2024-03-30 17:08:46 +0800

    feat: add BlogPost model

--- BlogMaker/blog_backend/models.py
+++ BlogMaker/blog_backend/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+    """
+    Model for a blog post.
+    """
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def snippet(self):
+        """
+        Returns the first 50 characters of the content if the content is longer than 50 characters.
+        """
+        return self.content[:50] if len(self.content) > 50 else self.content
