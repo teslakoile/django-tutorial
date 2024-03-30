6388112982df39ea1156715015f422d2dc79eced
Kyle Nathan Naranjo <63973445+teslakoile@users.noreply.github.com>
2024-03-30 21:14:09 +0800

    feat: add url paths for BlogPost list and detail views

---
+++ BlogMaker/blog_backend/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path(
+        'blogposts', 
+        views.BlogPostListView.as_view(), 
+        name='blogpost-list'
+    ),
+    path(
+        'blogposts/<int:blog_post_id>', 
+        views.BlogPostDetailView.as_view(), 
+        name='blogpost-detail'
+    ),
+]
